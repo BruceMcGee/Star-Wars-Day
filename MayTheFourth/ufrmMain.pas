0a52595cb86ef5a43d5e86c784aa116a133611e3
BruceMcGee <20337628+BruceMcGee@users.noreply.github.com>
2020-05-04 02:25:59 -0400

    Add files via upload

---
+++ MayTheFourth/ufrmMain.pas
@@ -0,0 +1,34 @@
+unit ufrmMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Types3D,
+  System.Math.Vectors, FMX.Ani, FMX.MaterialSources, FMX.Objects3D,
+  FMX.Controls3D, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Viewport3D;
+
+type
+  TfrmMain = class(TForm)
+    Viewport3D1: TViewport3D;
+    lblAttribution: TLabel;
+    lblBrand: TLabel;
+    Light1: TLight;
+    Model3D1: TModel3D;
+    Model3D1Mat01: TLightMaterialSource;
+    FloatAnimation1: TFloatAnimation;
+    lblTitle: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
